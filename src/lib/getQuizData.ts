cd95d567aff40f225b34a91c7ae30ddb2c970725
Maimoon Hossain <ashikasitosh740@gmail.com>
2024-01-07 21:58:40 +0600

    simple change

--- src/lib/getQuizData.ts
+++ src/lib/getQuizData.ts
@@ -2,7 +2,7 @@ import prisma from './prisma';
 
 export async function getQuizData(id: any) {
   console.log('prisma', prisma);
-  // Fetch available quizzes from Prisma
+
   const quizId = parseInt(id);
   console.log(quizId);
   const quiz = await prisma.quiz.findUnique({
